ed82c60660fb1d7a7ddefaae5bf3f7bf040cd9d4
Serhii <example@gmail.com>
2022-11-11 17:30:34 +0200

    Second practice

--- js/script.js
+++ js/script.js
@@ -10,12 +10,34 @@ const personalMovieDB = {
     privat: false
 };
 
-const oneOfTheFilms = prompt('Один з останніх фільмів?', ''),
-      rate = prompt('На скільки ви оціните його?', ''),
-      oneOfTheFilmsTwo = prompt('Один з останніх фільмів?', ''),
-      rateTwo = prompt('На скільки ви оціните його?', '');
+for (let i = 0; i< 2; i++){
+    const oneOfTheFilms = prompt('Один з останніх фільмів?', ''),
+          rate = prompt('На скільки ви оціните його?', '');
+    
+    if (oneOfTheFilms != '' && rate != '' && 
+        oneOfTheFilms != null && rate != null && 
+        oneOfTheFilms != undefined && rate != undefined && 
+        oneOfTheFilms.length < 10 && rate < 10) {
+        personalMovieDB.movies[oneOfTheFilms] = rate;
+        console.log('done!');
+    }
+    else {
+        console.log('error!');
+        i--;
+    }   
+}
 
-personalMovieDB.movies[oneOfTheFilms] = rate;
-personalMovieDB.movies[oneOfTheFilmsTwo] = rateTwo;
+if (personalMovieDB.count < 10) {
+    console.log('Переглянуто доволі мало фільмів');
+}
+else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30 ) {
+    console.log('Ви класичний глядач');
+}
+else if (personalMovieDB.count > 30 ) {
+    console.log('Ви кіноман!');
+}
+else{
+    console.log('error!');
+}
 
 console.log(personalMovieDB);
